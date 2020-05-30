109dedd44242df0bb9693458572f707a9fcab1c5
IDS-J_Sumita <39368863+SUMITAsatoru@users.noreply.github.com>
2020-05-30 22:34:43 +0900

    Initial commit

---
+++ demo/view/index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>デモ</title>
+    <link rel="stylesheet" type="text/css" href="../../common/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../../common/css/common.css">
+    <script src="../../common/js/bootstrap.min.js"></script>
+    <script src="../../common/js/common.js"></script>
+</head>
+<body>
+<script>
+    $(function () {
+        $("#common").load("../../common/view/common.html");
+    });
+</script>
+<div id="common"></div>
+
+</body>
+</html>
