79e7ab5b9c5ce75c19c1d4e549d2f95efc420170
IDS-Fukasaku <fukasaku@ids.co.jp>
2020-06-02 15:01:39 +0900

    change

--- src/views/FukasakuView.php
+++ src/views/FukasakuView.php
@@ -48,7 +48,7 @@
                                   placeholder="名かな"  value="<?= (isset($_POST['first_name_kana'])) ? $_POST['first_name_kana'] : '' ?>">
                         </div>
                         <div class="form-row">
-                           <div class="form -group col-md-3">
+                           <div class="form-group col-md-3">
                                <input type="number" id="emp_num" class="form-control form-control-sm" name="emp_num"
                                        placeholder="社員番号" min="0"  
                                        value="<?= (isset($_POST['emp_num'])) ? $_POST['emp_num'] : '' ?>">
