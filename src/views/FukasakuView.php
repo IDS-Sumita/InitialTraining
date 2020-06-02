668d6a4d9782056b8918844704f281c9213553bd
IDS-Fukasaku <fukasaku@ids.co.jp>
2020-06-02 15:07:28 +0900

    change

--- src/views/FukasakuView.php
+++ src/views/FukasakuView.php
@@ -50,7 +50,7 @@
                         <div class="form-row">
                            <div class="form-group col-md-3">
                                <input type="number" id="emp_num" class="form-control form-control-sm" name="emp_num"
-                                       placeholder="社員番号" min="0"  
+                                       placeholder="社員番号" min="0"  max="11"
                                        value="<?= (isset($_POST['emp_num'])) ? $_POST['emp_num'] : '' ?>">
                            </div>
                     </div>
