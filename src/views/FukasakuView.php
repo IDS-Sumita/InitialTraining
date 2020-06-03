21c6985412215ec666a9bcd20ddf75f1b6dc85bd
IDS-Fukasaku <fukasaku@ids.co.jp>
2020-06-03 11:26:36 +0900

    変更

--- src/views/FukasakuView.php
+++ src/views/FukasakuView.php
@@ -20,16 +20,16 @@
           <div class="card-header" id="headingOne">
               <div class="col">検索条件</div>
               <div class="col text-right">
-                    <button type="button" class="btn btn-info p-0 text-right" id="serach-header"
-                            data-traggle="collspace" data-target="#collspaceOne" aria-expanded="true"
-                             aria-controls="collspaceOne">▼閉じる
+                    <button type="button" class="btn btn-info p-0 text-right" id="search-header"
+                            data-toggle="collapse" data-target="#collapseOne" aria-expanded="true"
+                             aria-controls="collapseOne">▼閉じる
                     </button>           
                 </div>
           </div>
 
-            <div id="collspaceOne" class="collspace show" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
                 <div class="card-body pb-0">
-                  <form action="<?= BASEURL ?>/FukasakuController/serach" method="post">
+                  <form action="<?= BASEURL ?>/FukasakuController/search" method="post">
                     <div class="form-row">
                        <div class="form-group col-md-3">
                            <input type="text" id="last_name" class="form-control form-control-sm" name="last_name"
@@ -134,6 +134,49 @@
             </div>
       </div>
     </div>
+    <div class="row mt-4 mb-2">
+        <div class="col-md-6">検索結果：<?= count($this->users); ?>件</div>
+        <div class="col-md-6 text-right">
+            <button type="button" class="btn btn-lg btn-outline-info px-2" data-toggle="modal" data-target="#regsterModal">社員登録</button>
+        </div>
+    </div>
+    <table class="table table-sm table-hover"> 
+        <thead class="thead-light">
+        <tr>
+          <th scope="col">社員番号</th>
+          <th scope="col">名前</th>
+          <th scope="col">名前カナ</th>
+          <th scope="col">部署</th>
+          <th scope="col">役職</th>
+          <th scope="col">連絡先</th>
+          <th scope="col"></th>
+          <th scope="col"></th>                          
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach($this->user as $user): ?>
+            <tr>
+               <td class="pl-4"<?=$user->emp-num ?>></td>
+               <td><?=$user->last_name."\t".$user->last_name_kana ?></td>
+               <td><?=$user->first_name."\t".$user->first_name_kana ?></td>
+               <td><?=$this->department[$user->department_id] ?></td>
+               <td><?=$this->position[$user->position_id] ?></td>
+               <td><?=$user->phone_num ?></td>
+               <td class="text-center">
+                  <button class="btn btn-sm btn-outline-info" type="button" data-toggle="modal"
+                          data-target="#editModal" value="<?=$user->usre_id ?>">編集
+                   </button>
+                </td>
+                <td class="text-center">
+                   <button type="button" class="btn btn-sm btn-outline-primary"
+                    value="<?=$user->user_id ?>">
+                   削除</button>
+                </td>
+            </tr>
+        <?php endforeach;?>
+       </tbody>
+    </tabele>
+    <?php include 'fukasakuModal.php'; ?>
 </main>
 </body>
 </html>
